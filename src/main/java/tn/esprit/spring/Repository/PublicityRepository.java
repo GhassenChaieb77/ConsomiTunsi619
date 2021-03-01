9ef9268a5414e020b2c9a0cbe4bbf6e7a279e9a1
Maissa Belfekih <60753308+MaissaBelfekih1@users.noreply.github.com>
2021-03-01 16:46:02 +0100

    Maissa

---
+++ src/main/java/tn/esprit/spring/Repository/PublicityRepository.java
@@ -0,0 +1,11 @@
+package tn.esprit.spring.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import tn.esprit.spring.Entities.*;
+
+@Repository
+public interface PublicityRepository extends CrudRepository<Publicity, Long>{
+
+}
--- src/main/java/tn/esprit/spring/Repository/test.java
+++
@@ -1,5 +0,0 @@
-package tn.esprit.spring.Repository;
-
-public class test {
-
-}
