ac33e6b2eacafd79691e19ee912ae3b3fd9ce1a9
khaoulaMekni <khaoula.mekni@sprit.tn>
2021-04-21 14:53:46 +0100

    khaoula

---
+++ src/main/java/tn/esprit/configuration/JwtResponse.java
@@ -0,0 +1,65 @@
+package tn.esprit.configuration;
+import java.util.List;
+
+import tn.esprit.spring.Entities.Role;
+import tn.esprit.spring.Entities.User;
+
+public class JwtResponse {
+	private String token;
+	private String type = "Bearer";
+	private Long id;
+	private String email;
+	private String role;
+	private User u;
+
+
+	public JwtResponse(String accessToken, Long id, String email, String role) {
+		this.token = accessToken;
+		this.id = id;
+		this.email = email;
+		this.role = role;
+	}
+
+	
+	public String getAccessToken() {
+		return token;
+	}
+
+	public void setAccessToken(String accessToken) {
+		this.token = accessToken;
+	}
+
+	public String getTokenType() {
+		return type;
+	}
+
+	public void setTokenType(String tokenType) {
+		this.type = tokenType;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+}
